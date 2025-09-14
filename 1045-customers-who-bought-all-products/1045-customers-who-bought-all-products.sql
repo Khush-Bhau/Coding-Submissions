e3ac39c895d6ff4d2220ed62e008cfc68f09abda
Khush-Bhau <149191425+Khush-Bhau@users.noreply.github.com>
2025-09-14 22:06:14 +0530

    Time: 609 ms (33.21%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,5 +1,4 @@
-SELECT c.customer_id
-FROM Customer c
-CROSS JOIN (SELECT COUNT(*) AS total FROM Product) t
-GROUP BY c.customer_id
-HAVING COUNT(DISTINCT c.product_key) = MAX(t.total);
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+having count(distinct product_key) = (select count(*) from product)
