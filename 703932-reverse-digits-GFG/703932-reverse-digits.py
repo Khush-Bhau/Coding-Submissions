effcde7344804894e065c21061df9348c20f420e
Khush-Bhau <149191425+Khush-Bhau@users.noreply.github.com>
2025-10-11 14:37:57 +0530

    Added solution - LeetHub

---
+++ 703932-reverse-digits-GFG/703932-reverse-digits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseDigits(self, n):
+        rev = 0
+        while n != 0:
+            r = n % 10
+            rev = rev * 10 + r
+            n //= 10
+        return rev
