2d350444428a3b1cc722871de46bed1dd50b0779
Khush-Bhau <149191425+Khush-Bhau@users.noreply.github.com>
2025-10-24 17:41:38 +0530

    Added solution - LeetHub

---
+++ 7884-python-division-Hackerrank/7884-python-division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Printing the result of integer division
+    print(a // b)
+    
+    # Printing the result of float division
+    print(a / b)
