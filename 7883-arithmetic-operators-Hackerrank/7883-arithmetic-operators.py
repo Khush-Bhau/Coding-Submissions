65cc53514ae7d85fe043ecdf85c6118ceb2ef16e
Khush-Bhau <149191425+Khush-Bhau@users.noreply.github.com>
2025-10-24 17:41:27 +0530

    Added solution - LeetHub

---
+++ 7883-arithmetic-operators-Hackerrank/7883-arithmetic-operators.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Printing the sum of the two numbers
+    print(a + b)
+    
+    # Printing the difference of the two numbers
+    print(a - b)
+    
+    # Printing the product of the two numbers
+    print(a * b)
+
